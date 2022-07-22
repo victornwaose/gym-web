907219675348a7d8cb7ab9c0c9b74e641c6ecdf5
nwaose victor <nwaosevictor1234@gmail.com>
2022-07-22 22:12:37 +0100

    added NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../../assets/images/download.jpg";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+    return (
+        <div className="w-[90%] ml-auto mr-auto">
+            <div className="flex justify-between">
+                <Link to="/">
+                    <img src={Logo} className="w-[48px]  h-12" />
+                </Link>
+                <div className="flex justify-between">
+                    <div>
+                        <Link
+                            to="/"
+                            style={{
+                                textDecoration: "none",
+                                color: "#3a1212",
+                                borderBottom: "3px solid #FF2625",
+                            }}
+                        >
+                            Home
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            to="/"
+                            style={{
+                                textDecoration: "none",
+                                color: "#3a1212",
+                                borderBottom: "3px solid #FF2625",
+                            }}
+                        >
+                            Exercise
+                        </Link>
+                    </div>{" "}
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default NavBar
+export default NavBar;
