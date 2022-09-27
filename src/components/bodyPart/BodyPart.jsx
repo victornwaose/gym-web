19b217d1ce232df470b4a7be7a3bb0cc2370f3da
nwaose victor <nwaosevictor1234@gmail.com>
2022-09-27 01:22:54 +0100

    added library

--- src/components/bodyPart/BodyPart.jsx
+++ src/components/bodyPart/BodyPart.jsx
@@ -2,10 +2,33 @@ import React from "react";
 
 import Icon from "../../assets/icons/gym.png";
 
-const BodyPart = ({ item, setBody }) => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
     return (
-        <div>
-            <img src={Icon} alt="dumbbell" />
+        <div
+            className={`flex justify-center cursor-pointer mt-8 align-center, ${
+                bodyPart === item
+                    ? {
+                          borderTop: "4px solid #ff2625",
+                          backgroundColor: "#fff",
+                          borderBottomLeftRadius: "20px",
+                          width: "270px",
+                          height: "280px",
+                          gap: "20px",
+                      }
+                    : {
+                          background: "#fff",
+                          borderBottomLeftRadius: "20px",
+                          width: "270px",
+                          height: "280px",
+                          gap: "20px",
+                      }
+            }`}
+        >
+            <img
+                src={Icon}
+                alt="dumbbell"
+                style={{ width: "40px", height: "40px" }}
+            />
         </div>
     );
 };
