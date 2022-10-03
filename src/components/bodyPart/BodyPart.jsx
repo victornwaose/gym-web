be8d3a4102eec7592b54435e7d78d66bc19a1fb1
nwaose victor <nwaosevictor1234@gmail.com>
2022-10-03 23:59:26 +0100

    changes

--- src/components/bodyPart/BodyPart.jsx
+++ src/components/bodyPart/BodyPart.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Stack } from "@mui/system";
+import { Typography } from "@mui/material";
 
 import Icon from "../../assets/icons/gym.png";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+    console.log(item);
     return (
         <Stack
             type="button"
             alignItems="center"
             justify-content="center"
-            className="bodyPart-card mt-5 w-[80%] ml-auto mr-auto"
+            className="bodyPart-card mt-5 w-[80%] h-full sml-auto mr-auto"
             sx={{
                 borderTop: bodyPart === item ? "4px solid #ff2625" : "",
                 backgroundColor: "#fff",
                 borderBottomLeftRadius: "20px",
-                width: "270px",
-                height: "28px",
+                width: "170px",
+                height: "58px",
                 cursor: "pointer",
-                gap: "47px",
+                gap: "17px",
             }}
         >
             <img
@@ -25,6 +27,14 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
                 alt="dumbbell"
                 style={{ width: "80px", height: "40px" }}
             />
+            <h1
+                fontSize="24px"
+                fontWeight="bold"
+                textTransform="upperCase"
+                color="black"
+            >
+                {item}
+            </h1>
         </Stack>
     );
 };
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,20 +1,48 @@
 import React from "react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-
 import { Box } from "@mui/system";
 
+import Left from "../../assets/icons/left-arrow.png";
+import Right from "../../assets/icons/right-arrow.png";
 import BodyPart from "../bodyPart/BodyPart";
 
 const Slider = ({ data, bodyPart, setBodyPart }) => {
+    const LeftArrow = () => {
+        const { scrollPrev } = React.useContext(VisibilityContext);
+
+        return (
+            <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+                <img src={Left} alt="leftArrow" />
+            </Arrow>
+        );
+    };
+
+    const RightArrow = () => {
+        const { isLastItemVisible, scrollNext } =
+            React.useContext(VisibilityContext);
+
+        return (
+            <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+                <img src={Right} alt="leftArrow" />
+            </Arrow>
+        );
+    };
+
     console.log(bodyPart);
+
     return (
-        <ScrollMenu className=" w-[80%] ml-auto mr-auto">
+        <ScrollMenu
+            className=" w-[80%] ml-auto mr-auto"
+            RightArrow={Right}
+            LeftArrow={Left}
+        >
             {data?.map((item) => (
                 <Box
                     width="400px"
                     key={item.id || item}
                     itemId={item.id || item}
                     title={item.id || item}
+                    m="0 40px"
                 >
                     <BodyPart
                         item={item}
